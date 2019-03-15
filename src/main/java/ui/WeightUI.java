364b2f17feb573c02d6346074c5628eb547f6333
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-03-15 16:51:14 +0100

    Create WeightUI.java
    Barebones WeightUI class file created...
    Highfive me! xd :ok_hand:

---
+++ src/main/java/ui/WeightUI.java
@@ -0,0 +1,14 @@
+package ui;
+
+import functionality.IUserFunctionality;
+
+public class WeightUI implements IUI{
+    private IUserFunctionality func;
+
+    // TODO: Will be VERY 'barebones' until func layer is fleshed out
+
+    @Override
+    public void showMenu() {
+
+    }
+}
