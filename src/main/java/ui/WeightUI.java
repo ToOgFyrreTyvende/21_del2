9344f6a9d7a0234d5bdfc0579757f4f5fc307162
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-03-18 11:56:04 +0100

    Update WeightUI.java
    Finished first draft of console logging + weight UI
    :ok_hand:
    
    Switched scan.next() to scan.nextLine()

--- src/main/java/ui/WeightUI.java
+++ src/main/java/ui/WeightUI.java
@@ -32,7 +32,7 @@ public class WeightUI implements IUI {
             } catch (NumberFormatException e){
                 System.out.println("Ugyldigt tal: " + opNumStr);
                 System.out.println("Tryk enter for at starte forfra...");
-                scan.next();
+                scan.nextLine();
                 showMenu();
                 break;
             }
@@ -47,7 +47,7 @@ public class WeightUI implements IUI {
             } else {
                 System.out.println("Ingen bruger fundet med nummer: " + opNumStr);
                 System.out.println("Tryk enter for at starte forfra...");
-                scan.next();
+                scan.nextLine();
                 showMenu();
                 break;
             }
@@ -63,6 +63,12 @@ public class WeightUI implements IUI {
                 batchNum = Integer.parseInt(batchNumStr);
             } catch (NumberFormatException e){
             }
+            if (batchNum == 1234){
+                System.out.println("Batch genkendt.");
+                System.out.println(batchNum + ": salt");
+            } else {
+                System.out.println("Batch ikke genkendt... Fortsætter");
+            }
 
             System.out.println("Trin 7:");
             System.out.println("Operatøren instrueres om, at vægten skal være ubelastet:");
@@ -75,8 +81,72 @@ public class WeightUI implements IUI {
             System.out.println("Trin 10:");
             System.out.println("Operatøren instrueres om, at placere tara på vægten:");
             wFunc.getConfirmation("Placer tom beholder på vægt, tryk ok");
+            System.out.println("Trin 11: Operatør kvitterer");
+
+            System.out.println("Trin 12: Beholders vægt registreres");
+            String taraWeightStr = wFunc.getWeight();
+            double taraWeight = 0d;
+            try{
+                taraWeight = Double.parseDouble(taraWeightStr);
+            } catch (NumberFormatException e){
+                e.printStackTrace();
+                System.out.println("wtf happened...?");
+                System.out.println("... Noget gik meget galt... " +
+                        "tryk enter for at starte forfra...");
+                scan.nextLine();
+            }
+            System.out.println("Vægt registreret: " + taraWeight);
+
+            System.out.println("Trin 13: Vægtens tareres");
+            wFunc.taraWeight();
+
+            System.out.println("Trin 14:");
+            System.out.println("Operatør instrueres i at placere netto på vægten:");
+            wFunc.getConfirmation("Placer netto på vægt, tryk ok");
+            System.out.println("Trin 15: Op kvitterer");
+
+            System.out.println("Trin 16: Nettovægt registreres:");
+            String nettoWeightStr = wFunc.getWeight();
+            double nettoWeight = 0d;
+            try{
+                nettoWeight = Double.parseDouble(nettoWeightStr);
+            } catch (NumberFormatException e){
+                e.printStackTrace();
+                System.out.println("Wtf happened...?");
+                System.out.println("... Noget gik meget galt... " +
+                        "tryk enter for at starte forfra...");
+                scan.nextLine();
+            }
+            System.out.println("Vægt registreret: " + nettoWeight);
+
+            System.out.println("Trin 17: Vægt tareres");
+            wFunc.taraWeight();
+
+            System.out.println("Trin 18: Op instrueres i at fjerne brutto fra vægt");
+            wFunc.getConfirmation("Fjern brutto fra vægt, tryk ok");
+            System.out.println("Trin 19: Op: ok");
+
+            System.out.println("Trin 20: Bruttovægt registreres (negativ)");
+            String bruttoWeightStr = wFunc.getWeight();
+            double bruttoWeight = 0d;
+            try{
+                bruttoWeight = Double.parseDouble(bruttoWeightStr);
+            } catch (NumberFormatException e){
+                e.printStackTrace();
+                System.out.println("Wtf happened...?");
+                System.out.println("... Noget gik meget galt... " +
+                        "tryk enter for at starte forfra...");
+                scan.nextLine();
+            }
+            System.out.println("Vægt registreret: " + bruttoWeight);
+
+            System.out.println("Trin 21: Udskriv OK på vægt");
+            wFunc.getConfirmation("OK");
+            System.out.println("Trin 22: Op: ok");
+
+            System.out.println("Trin 23: tara vægt");
+            wFunc.taraWeight();
+            System.out.println("Vægt efter tara: " + wFunc.getWeight());
         }
-
-
     }
 }
